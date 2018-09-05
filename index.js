82d559bd16a6482997fbfe6b8764922886510986
Ben <bmtben@gmail.com>
2018-09-06 00:07:32 +1000

    First commit

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ var express = require('express')
 var ngrok = require('ngrok')
 var bodyParser = require('body-parser')
 var app = express()
+
 const serverPort = 8091
 var savedConfig = {}
+app.use(express.static('public'))
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-reminders.login('Device Name', 'username', 'password', function(error, response, config){
+reminders.login('Ben\'s Echo dot', 'bmtben@gmail.com', 'Mike!123', function(error, response, config){
   savedConfig = config
   console.log(response)
 })
 
 app.post('/alexa-reminders', urlencodedParser, function (req, res) {
   var reminder = req.body.reminder;
-  reminders.setReminder(reminder, null, savedConfig, function(error, response){
+  var date = req.body.datetime;
+  reminders.setReminder(reminder, date, savedConfig, function(error, response){
+    console.log(`reminder: "${reminder}" has been set for ${date}`)
     res.send(response)
   })
 })
 
 app.listen(serverPort, function () {
   ngrok.connect(serverPort, function (err, url) {
-    console.log('External endpoint:')
-    console.log('curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" ' + url + '/alexa-reminders')
-    console.log('Internal endpoint:')
-    console.log('curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" ' + 'http://localhost:' + serverPort + '/alexa-reminders')
+    console.log('External endpoint: ' + url)
+    console.log('Internal endpoint: http://localhost:' + serverPort)
   })
+  console.log('Example app listening on port 8091')
 })
